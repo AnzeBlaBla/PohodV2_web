07255001eb38a8a82b58517e0cc0595c7767cb90
gorazdkotnik <gorazdkotnik05@gmail.com>
2022-04-08 12:34:54 +0200

    fixed alert styling

--- src/components/UI/Alert.js
+++ src/components/UI/Alert.js
@@ -4,7 +4,7 @@ function Alert({ title, text, type }) {
   const getAlertClass = () => {
     switch (type) {
       case 'success':
-        return 'bg-orange-100 border-green-500 text-green-700';
+        return 'bg-green-100 border-green-500 text-green-700';
       case 'error':
         return 'bg-red-100 border-red-500 text-red-700';
       case 'info':
