4b28664e1807e8e939172f7ab0c99a39abb3311f
Gorazd Kotnik <59970689+gorazdkotnik@users.noreply.github.com>
2022-05-13 19:16:01 +0200

    modal improvements

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -11,8 +11,16 @@ export default function Modal() {
       {dialog &&
         ReactDom.createPortal(
           <div className="fixed top-0 left-0 w-screen h-screen overflow-hidden z-[998]">
-            <div className="fixed top-0 left-0 flex justify-center items-center w-full h-full bg-black/75 p-5">
-              <div className="bg-white py-5 px-10 w-full max-w-xl">
+            <div
+              className="fixed top-0 left-0 flex justify-center items-center w-full h-full bg-black/75 p-5"
+              onClick={e => {
+                setDialog(null);
+              }}
+            >
+              <div
+                className="bg-white py-5 px-10 w-full max-w-xl"
+                onClick={e => e.stopPropagation()}
+              >
                 <h2 className="mb-10 mt-5 text-2xl font-bold">
                   {dialog.title}
                 </h2>
@@ -20,7 +28,9 @@ export default function Modal() {
                 <div className="flex items-center justify-end">
                   <button
                     className="button-outline"
-                    onClick={() => setDialog(null)}
+                    onClick={e => {
+                      setDialog(null);
+                    }}
                   >
                     Vredu
                   </button>
--- src/components/UI/PageMenu.js
+++ src/components/UI/PageMenu.js
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 function PageMenu({ labels, links }) {
   const menuLinkStyles = ({ isActive = false }) =>
     isActive
-      ? 'button-outline text-white bg-blue-700 mr-5'
-      : 'button-outline mr-5';
+      ? 'button-outline text-white bg-blue-700 mr-5 text-sm md:text-base'
+      : 'button-outline mr-5 text-sm md:text-base';
 
   return (
     <div className="my-5 mt-10 flex  items-center">
