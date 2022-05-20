e6b7a9cc7f1a9d68798f6f99f1ffde655282008a
gorazdkotnik <gorazdkotnik05@gmail.com>
2022-05-20 11:52:36 +0200

    added teacher group tracking

--- src/components/home/TeacherHome.js
+++ src/components/home/TeacherHome.js
@@ -1,25 +1,87 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+import { useGlobalContext } from '../../context/GlobalContext';
 
 import { request } from '../../utils/functions';
 
-import HomeTemplate from './HomeTemplate';
+import Container from '../UI/Container';
+import Card from '../UI/Card';
+import Map from '../UI/Map';
+
+import SelectEvent from '../leaderboard/SelectEvent';
 
 function TeacherHome() {
+  const { setShowLoadingSpinner, setDialog } = useGlobalContext();
+
+  const [events, setEvents] = useState([]);
+  const [points, setPoints] = useState([]);
+
+  const [selectedEvent, setSelectedEvent] = useState('');
+
   useEffect(() => {
-    request('/events/37/tracking')
+    setShowLoadingSpinner(true);
+    request('/events')
       .then(data => {
-        console.log(data);
+        setShowLoadingSpinner(false);
+        setEvents(data);
       })
       .catch(err => {
-        console.log(err);
+        setShowLoadingSpinner(false);
+        setDialog({
+          title: 'Napaka pri pridobivanju dogodkov',
+          text: 'Prišlo je do napake pri pridobivanju dogodkov. Poskusite znova.',
+        });
       });
-  }, []);
+  }, [setShowLoadingSpinner, setDialog]);
+
+  const eventOnChangeHandler = event => {
+    setSelectedEvent(event.target.value);
+  };
+
+  useEffect(() => {
+    if (selectedEvent) {
+      setShowLoadingSpinner(true);
+      request(`/events/${selectedEvent}/tracking`)
+        .then(data => {
+          setShowLoadingSpinner(false);
+
+          setPoints(
+            data.map(group => ({ ...group.point, name: group.group.name }))
+          );
+        })
+        .catch(err => {
+          setShowLoadingSpinner(false);
+          setDialog({
+            title: 'Napaka pri pridobivanju obiskanih točk skupin',
+            text: 'Prišlo je do napake pri pridobivanju obiskanih točk skupin. Poskusite znova.',
+          });
+        });
+    }
+  }, [selectedEvent, setShowLoadingSpinner, setDialog]);
 
   return (
-    <HomeTemplate
-      title="Pohod V2 - Učitelj"
-      text="Aplikacija za organizacijo in izvedbo orientacijskega pohoda."
-    />
+    <>
+      {points.length < 1 && (
+        <Container mode="page">
+          <Card>
+            {/* Title */}
+            <h1 className="card-primary-title">Zadnje obiskane točke skupin</h1>
+            {/* Text */}
+            <p className="card-primary-text">
+              Prikaz zadnjih obiskanih točk skupin.
+            </p>
+            {/* Select List */}
+            <SelectEvent
+              events={events}
+              selectedEvent={selectedEvent}
+              eventOnChangeHandler={eventOnChangeHandler}
+            />
+            <hr className="my-5"></hr>
+          </Card>
+        </Container>
+      )}
+      {points.length > 0 && <Map points={points} />}
+    </>
   );
 }
 
