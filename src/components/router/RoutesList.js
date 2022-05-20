959b8be14b579577d146cf5dd2c934d70d777fe0
gorazdkotnik <gorazdkotnik05@gmail.com>
2022-05-20 12:25:37 +0200

    optimized routes list

--- src/components/router/RoutesList.js
+++ src/components/router/RoutesList.js
@@ -3,19 +3,20 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from '../../pages/Home';
 import Login from '../../pages/Login';
-import Leaderboard from '../../pages/Leaderboard';
-import Events from '../../pages/events/Events';
-import Groups from '../../pages/groups/Groups';
-import NoPage from '../../pages/NoPage';
-import Profile from '../../pages/Profile';
-import Results from '../../pages/Results';
-import PointQuestions from '../../pages/PointQuestions';
 
 import Navbar from '../layouts/Navbar';
 
 import { userTypes } from '../../utils/consts';
 
 import useUser from '../../hooks/useUser';
+
+const Leaderboard = React.lazy(() => import('../../pages/Leaderboard'));
+const Events = React.lazy(() => import('../../pages/events/Events'));
+const Groups = React.lazy(() => import('../../pages/groups/Groups'));
+const NoPage = React.lazy(() => import('../../pages/NoPage'));
+const Profile = React.lazy(() => import('../../pages/Profile'));
+const Results = React.lazy(() => import('../../pages/Results'));
+const PointQuestions = React.lazy(() => import('../../pages/PointQuestions'));
 
 function RoutesList() {
   const { user } = useUser();
